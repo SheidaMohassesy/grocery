a4b5e513566c977599681e22ea0e52685ff24e64
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-10-26 22:07:21 +0200

    Add the basic logic

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 // ****** SELECT ITEMS **********
 
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitButton = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearButton = document.querySelector(".clear-btn");
 // edit option
 
+let editElement;
+let editFlag = false;
+let editID = "";
 // ****** EVENT LISTENERS **********
+// submit form
+
+
 
 // ****** FUNCTIONS **********
-
+const addItem = (e) => {
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    console.log(id);
+};
+form.addEventListener("submit", addItem);
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
