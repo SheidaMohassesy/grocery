cda1ad7000aca42db7bf581d531faf30a65efe97
Sheida Mohassesy <sheida.mohassesi@gmail.com>
2022-10-29 22:07:09 +0200

    Add the main function

--- app.js
+++ app.js
@@ -24,16 +24,53 @@ const addItem = (e) => {
     const value = grocery.value;
     const id = new Date().getTime().toString();
     if(value && !editFlag){
-        console.log("adding item to the list");
+        const element = document.createElement("article");
+    // add class
+    element.classList.add("grocery-item");
+    // add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    
+    // element.setAttribute("data-id", id);
+    
+    // element.attributes["data-id"] = id;
+
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+      <button type="button" class="edit-btn">
+        <i class="fas fa-edit"></i>
+      </button>
+      <button type="button" class="delete-btn">
+        <i class="fas fa-trash"></i>
+      </button>
+    </div>`;
+    // append child
+    list.appendChild(element);
+    //display alert
+    displayAlert("item added to the list", "success");
+    // show container
+    container.classList.add("show-container");
     }
     else if(value && editFlag){
         console.log("editing");
     }
     else{
-        alertElement.textContent = "empty value";
+        displayAlert("please enter value", "danger");
     }
 };
 form.addEventListener("submit", addItem);
+
+// display alert
+const displayAlert = (text, action) => {
+    alertElement.textContent = text;
+    alertElement.classList.add(`alert-${action}`);
+    // remove alert
+    setTimeout(() => {
+        alertElement.textContent = "";
+        alertElement.classList.remove(`alert-${action}`);
+    }, 1000);
+};
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
